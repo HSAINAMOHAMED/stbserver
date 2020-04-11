f3a88858e05af276c314f2ae0fdfb03c2c2f30b0
HSAINAMOHAMED <mohamedhsaina@gmail.com>
2020-04-11 18:18:27 +0200

    page index & help

--- src/main/java/fr/univ/rouen/stbserver/StbserverApplication.java
+++ src/main/java/fr/univ/rouen/stbserver/StbserverApplication.java
@@ -2,12 +2,23 @@ package fr.univ.rouen.stbserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class StbserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StbserverApplication.class, args);
+	}
+
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry.addResourceHandler("/**").addResourceLocations("classpath:/images/","classpath:/templates/").setCachePeriod(0);
+		}
 	}
 
 }
--- src/main/java/fr/univ/rouen/stbserver/controllers/IndexController.java
+++ src/main/java/fr/univ/rouen/stbserver/controllers/IndexController.java
@@ -1,13 +1,19 @@
 package fr.univ.rouen.stbserver.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 public class IndexController {
 	@GetMapping("/")
 	public String index() {
-		return "Hello there !";
+		return "index.html";
+	}
+
+	@GetMapping(value = "/help")
+	public String help(ModelMap Model) {
+		return "stb.html";
 	}
 }
